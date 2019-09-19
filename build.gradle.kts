100fe44a8ee4106206938557881bf0be3f223272
Duncan Calvert <dcalvert@ihmc.us>
2019-09-19 16:53:09 -0500

    Upgrade to ihmc-build 0.18.5 and release 4.0.0-ihmc3

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+   id("us.ihmc.ihmc-build") version "0.18.5"
+   id("us.ihmc.log-tools") version "0.3.1"
+}
+
+ihmc {
+   group = "us.ihmc"
+   version = "4.0.0-ihmc3"
+   vcsUrl = "https://github.com/ihmcrobotics/jassimp-build"
+   openSource = true
+   maintainer = "Duncan Calvert"
+
+   configureDependencyResolution()
+   configurePublications()
+}
+
+ihmc.sourceSet("main").java.srcDirs("assimp/port/jassimp/jassimp/src/")
+
+mainDependencies {
+   compile("us.ihmc:ihmc-native-library-loader:1.1")
+}
